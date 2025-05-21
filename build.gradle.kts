076ec20e63793a59dd8614d26a2ac9b7f3ab6808
人間工作 <coolxiaom95@gmail.com>
2025-05-21 18:52:49 +0800

    use kotlin dsl build script

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    java
+}
+
+val targetJavaVersion = 8
+group = "studio.trc"
+version = "1.3.2.1"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://jitpack.io/")
+}
+
+@Suppress("VulnerableLibrariesLocal")
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("net.milkbowl.vault:VaultAPI:1.7")
+    compileOnly("com.github.MascusJeoraly:LanguageUtils:1.9")
+
+    implementation("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.20")
+}
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks {
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand("version" to version)
+            include("plugin.yml")
+        }
+    }
+}
